8bec14bfce8287caa9b46136324850e128eeeabd
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-22 14:27:03 +0100

    bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,6 @@
     <title>hello laravel</title>
 </head>
 <body>
-    <h1>hello world</h1>
+    <h1>{{ $greeting }}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $greeting = 'Ciao boolean e ciao Laravel';
+    return view('home' , compact('greeting'));
 });
